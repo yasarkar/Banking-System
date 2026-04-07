41cc960d2b3118212a6c670b19792f20bdeb78d0
Yaşar Kar <20222022429@std.nisantasi.edu.tr>
2026-04-07 19:43:44 +0300

    Update DatabaseManager.java
    "Güvenlik güncellemesi: Veritabanı şifreleri gizlendi"

--- BankacilikSistemi/DatabaseManager.java
+++ BankacilikSistemi/DatabaseManager.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DatabaseManager {
 
     private static final String url = "jdbc:mysql://localhost:3306/bankasistemi";
-    private static final String user = "yasar";
-    private static final String dbPassword = "yakhhkay34";
+    private static final String user = System.getenv("DB_USER");
+    private static final String dbPassword = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, dbPassword);
